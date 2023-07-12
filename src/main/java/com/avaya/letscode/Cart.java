64510b344441916bd0274fb7896c86e4ae6051ee
vbholwani <vbholwani@avaya.com>
2023-07-12 17:49:13 +0530

    Rearranged methods in the class - all the instance variables at the top, then constructor, then all other methods. Moved all variants of AddItem() together, all variants of getQuantity() together.

--- src/main/java/com/avaya/letscode/Cart.java
+++ src/main/java/com/avaya/letscode/Cart.java
@@ -7,28 +7,47 @@ import java.util.Map;
 
 public class Cart {
     //List of items in the cart with respective unit quantity
-    private Map<Product, CartItem> items;       
+    private final Map<Product, CartItem> items;
     
     //List of offers applicable to the cart
     // Map<String, Offer> cartOffers;
     private List<Offer> cartOffers;
 
-
     int quantity = 0;       //Total number of items in cart
-    public boolean isEmpty() {
-        return quantity == 0;
-    }
 
     Cart(){
         this.items = new HashMap<>();
         this.cartOffers = new ArrayList<>();
     }
+
+    public boolean isEmpty() {
+        return quantity == 0;
+    }
+
     public void AddItem(Product product) {
         this.AddItem(product, 1);
     }
 
+    public void AddItem(Product product, int quantity) {
+        this.AddItem(product, quantity,"");
+    }
+
+    public void AddItem(Product product, int quantity, String offer) {
+        this.quantity += quantity;
+        CartItem cartItem = new CartItem(product, quantity, Offer.create(offer));
+        this.items.merge(product, cartItem, CartItem::merge);
+    }
+
+    public void AddOffer(String offer) {
+        this.cartOffers.add(Offer.create(offer));
+    }
+
     public int getQuantity() {
         return quantity;
+    }
+
+    public int getQuantity(Product product) {
+        return this.items.containsKey(product) ? this.items.get(product).getQuantity() : 0;
     }
 
     public double getCartValue() {
@@ -44,24 +63,5 @@ public class Cart {
 
     public double getCartValueBeforeOffer() {
         return this.items.values().stream().mapToInt(CartItem::getCartItemValue).sum();
-    }
-
-    public void AddItem(Product product, int quantity) {
-        this.AddItem(product, quantity,"");
-    }
-
-    public int getQuantity(Product product) {
-        return this.items.containsKey(product) ? this.items.get(product).getQuantity() : 0;
-    }
-
-    public void AddItem(Product product, int quantity, String offer) {
-        this.quantity += quantity;
-
-        CartItem cartItem = new CartItem(product, quantity, Offer.create(offer));
-        this.items.merge(product, cartItem, CartItem::merge);
-    }
-
-    public void AddOffer(String offer) {
-        this.cartOffers.add(Offer.create(offer));
     }
 }
